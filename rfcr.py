a10df3d9588060b3bb9aeba3bb7196ca4591c618
cmcinroy <craig.mcinroy@marinerpartners.com>
2017-02-22 14:18:58 -0400

    augment most functions; add log() and speak(); add register dump.

--- rfcr.py
+++ rfcr.py
@@ -1,5 +1,8 @@
 # RFCR
-# Tracks attendance by recording 'enter' and 'leave' actions.
+# Tracks class attendance by recording 'enter' and 'leave' events.
+
+# import external modules
+from datetime import datetime
 
 # initialize a new, empty "dictionary" variable to store student ids/names
 students = {}
@@ -7,30 +10,44 @@ students = {}
 # initialize a new, empty "list" variable to store ids present
 idsPresent = []
 
+
 # - - - - - - - - - - - - - - - - - - - - - - - - -
 # Function: a student has entered the class
-def enter():
-    log(1)
-    print('{} entered the class.'.format(students.get(scan)))
-    idsPresent.append(scan)
+def enter(id):
+    idsPresent.append(id)
+    print('{} entered the class.'.format(students.get(id)))
+    speak('Hello, ' + students.get(id) + '.')
+    log(1, id)
+
 
 # - - - - - - - - - - - - - - - - - - - - - - - - -
 # Function: a student has left the class
-def leave():
-    log(0)
-    print('{} left the class.' .format(students.get(scan)))
-    idsPresent.remove(scan)
+def leave(id):
+    idsPresent.remove(id)
+    print('{} left the class.'.format(students.get(id)))
+    speak('Goodbye, ' + students.get(id) + '.')
+    log(0, id)
+
 
 # - - - - - - - - - - - - - - - - - - - - - - - - -
-# Function: log the action
-def log(opt):
-    # do nothing for now
-    opt = 0
-    
+# Function: log the event
+#   opt = 1 (enter) or  0 (leave)
+def log(opt, id):
+    # just print for now
+    print('log: {},{},{},{}'.format(datetime.now(), opt, id, len(idsPresent)))
+
 
 # - - - - - - - - - - - - - - - - - - - - - - - - -
+# Function: convert the text to "speech"
+def speak(text):
+    # just print for now
+    print('speak: ' + text)
+
+
+# = = = = = = = = = = = = = = = = = = = = = = = = =
 # Main program
-#
+# - - - - - - - - - - - - - - - - - - - - - - - - -
+
 # read student list from file into our dictionary
 with open('students.dat', 'r') as datafile:
     for line in datafile:
@@ -45,14 +62,19 @@ while not finished:
     # if someone pressed enter with no data, exit the loop
     if scan == '':
         finished = True
+    elif scan == '?':
+        # Dump class list
+        print('{} student(s) in class:'.format(len(idsPresent)))
+        for id in idsPresent:
+            print('  {}'.format(students[id]))
     else:
         # otherwise, check whether the card is registered
         if scan in students:
-            # if the student is already present, call the leave() function
+            # if the student is already present, this is a 'leave' event
             if scan in idsPresent:
-                leave()
-            # otherwise, call the enter() function
+                leave(scan)
+            # otherwise, this is an 'enter' event
             else:
-                enter()
+                enter(scan)
         else:
             print('Card {} is not registered for this class.'.format(scan))

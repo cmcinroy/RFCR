f233363a9e681c42a198486effa4aacaf686d154
cmcinroy <craig.mcinroy@marinerpartners.com>
2017-03-28 11:03:37 -0300

    lots of changes: save/load state, checksum on data file, language support for all messages, consolidate enter/exit actions into single function, calc time delta since last event.

---
+++ phrases.py
@@ -0,0 +1,64 @@
+#
+# Define phrases for all languages
+#
+phrases = {
+    'en': ('Goodbye', 'Hello', 
+        'left the class', 'entered the class', 
+        'absent for', 'present for',
+        'Please scan your card',
+        'Reset system (clear again to confirm)?',
+        'Card {} is not registered for this class.',
+        'Reading student data', ' done.',
+        'WARNING: previous state is being used, but student data has changed.',
+        '{} student(s) in class:',
+        'Class is empty.',
+        'Class is not empty.',
+        'System state will be saved.',
+        'Previous state found; system will be initialized from this prior state.',
+        'ERROR: cannot save state.',
+        'ERROR: cannot access previous state.',
+        'ERROR: cannot remove previous state file.',
+        'System has been reset to a clear initial state.',
+        'days', 'hours', 'minutes', 'seconds'
+        ),
+    'fr': ('Au revoir', 'Bienvenue', 
+        'a quitté la classe', 'a rejoint la classe', 
+        'absent pendant', 'présent pendant',
+        'Veuillez analyser votre carte',
+        'Réinitialiser le système (effacer à nouveau pour confirmer)?',
+        'La carte {} n''est pas enregistrée pour cette classe.',
+        'Traitement des données des élèves', ' fini.',
+        'AVERTISSEMENT: l''état précédent est utilisé, mais le fichier de données a changé.',
+        '{} Étudiant(s) dans la classe:',
+        'La classe est vide.',
+        'La classe n''est pas vide.',
+        'L''état du système sera enregistré.',
+        'Etat précédent trouvé; Système sera initialisé à partir de cet état antérieur.',
+        'ERREUR: Impossible d''enregistrer l''état.',
+        'ERREUR: impossible d''accéder à l''état précédent.',
+        'ERREUR: impossible de supprimer le fichier d''état précédent.',
+        'Le système a été réinitialisé à un état initial clair.',
+        'jours', 'heures', 'minutes', 'secondes'
+        )
+}
+PHRASE_ACTION = 2
+PHRASE_DELTA = 4
+PHRASE_INPUT = 6
+PHRASE_RESET_CONFIRM = 7
+PHRASE_NOTREG = 8
+PHRASE_DATA_READ = 9
+PHRASE_DATA_COMPLETE = 10
+PHRASE_WARN_DATA_CHECK = 11
+PHRASE_QUERY = 12
+PHRASE_STATE_EMPTY = 13
+PHRASE_STATE_NOT_EMPTY = 14
+PHRASE_STATE_SAVE = 15
+PHRASE_STATE_LOAD = 16
+PHRASE_ERR_STATE_SAVE = 17
+PHRASE_ERR_STATE_READ = 18
+PHRASE_ERR_STATE_FILE = 19
+PHRASE_RESET = 20
+PHRASE_DAYS = 21
+PHRASE_HRS = 22
+PHRASE_MINS = 23
+PHRASE_SECS = 24
--- rfcr.py
+++ rfcr.py
@@ -1,10 +1,17 @@
 # RFCR
-# Tracks class attendance by recording 'enter' and 'leave' events.
+# Radio Frequency Class Registrator
+#
+# Tracks class attendance by recording 'enter' and 'exit' events.
 
 # - - - - - - - - - - - - - - - - - - - - - - - - -
+# Import local modules
+from phrases import *
 # Import external modules
 from datetime import datetime
 from gtts import gTTS
+import hashlib
+import json
+from json import JSONDecoder, JSONEncoder
 import logging
 import os
 import subprocess
@@ -12,21 +19,37 @@ import subprocess
 # - - - - - - - - - - - - - - - - - - - - - - - - -
 # Global variables
 
-# Set name of temporary directory where sound files are stored
-gTemp = 'temp'
+# Set language (en/fr)
+language = 'en'
+
+# Set name of student data file
+DATA_FILE = 'students.dat'
+
+# Set name of temporary directory where state and sound files are stored
+TEMP_DIR = 'temp'
+
+# Set name of temporary file where current state is stored
+STATE_FILE = os.path.join(TEMP_DIR, 'state.json')
+
+# Initialize student data file checksum
+data_check = ''
 
 # Initialize a new, empty "dictionary" variable to store student ids/names
 students = {}
 
-# Initialize a new, empty "list" variable to store ids present
-idsPresent = []
+# Set the list of action names
+actions = ['exit', 'enter']
 
-# Set language (en/fr) and salutations
-gLang = 'en'
-salutations = {
-    'en': ('Goodbye','Hello'),
-    'fr': ('Au revoir','Bienvenue')
-}
+# Initialize dictionary variable to store absent/present ids (and the date/time of exit/entry)
+#   ids['exit'] will return a dictionary of students that have left (key=id, value=date/time of exit)
+#   ids['enter'] will return a dictionary of students that have entered (key=id, value=date/time of entry)
+# ids = {'exit': {}, 'enter': {}}
+ids = {key: {} for key in actions}
+# Create a "view" of student ids that are present in class
+ids_present = ids[actions[1]].keys()
+
+# Initialize dictionary to store current state
+state = {}
 
 # Create a logger to log events
 logger = logging.getLogger(__name__)
@@ -35,39 +58,120 @@ logger = logging.getLogger(__name__)
 # - - - - - - - - - - - - - - - - - - - - - - - - -
 # Function: Main script logic
 def main():
-    finished = False;
+    finished = False
+    confirm = False
     # Start a loop
     while not finished:
         # Accept input from RFID scanner (same as keyboard)
-        scan = input('\nPlease scan your card:\n')
+        scan = input('\n%s:\n' % (phrases.get(language)[PHRASE_INPUT]))
         # If someone pressed enter with no data, exit the loop
         if scan == '':
             finished = True
+        elif scan == '=':
+            # Clear everything? (if confirmed)
+            if confirm:
+                # Clear current state
+                clear_state()
+                confirm = False
+            else:
+                print(phrases.get(language)[PHRASE_RESET_CONFIRM])
+                confirm = True
         elif scan == '?':
-            # Dump class list
-            print('{} student(s) in class:'.format(len(idsPresent)))
-            for id in idsPresent:
-                print('  {}'.format(students[id]))
+            print_state()
+            confirm = False
         else:
             # Otherwise, check whether the card is registered
             if scan in students:
-                # If the student is already present, this is a 'leave' event
-                if scan in idsPresent:
-                    leave(scan)
-                # Otherwise, this is an 'enter' event
-                else:
-                    enter(scan)
+                # "Toggle" the current status of the student (present/absent)
+                toggle(scan, datetime.today())
             else:
-                print('Card {} is not registered for this class.'.format(scan))
+                print(phrases.get(language)[PHRASE_NOTREG].format(scan))
+            confirm = False
+    # Save current state before exit
+    save_state()
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - -
+# Function: Perform action (enter/exit) for the specified card id
+def toggle(id, event_time):
+    # Determine current status (present/absent)
+    current = id in ids_present
+    # Create a message for the action (student name + action phrase)
+    message = [students.get(id), phrases.get(language)[current + PHRASE_ACTION]]
+    # Remove from current list and get the previous action event time (if any)
+    prev_time = ids.get(actions[current]).pop(id, 0)
+    # Calculate difference in time between last action and current
+    if (prev_time != 0):
+        time_diff = event_time - prev_time
+        message.append("(%s %s)" % (phrases.get(language)[current + PHRASE_DELTA], format_timedelta(time_diff)))
+    # Flip status and update class register with event time
+    current = not current
+    ids.get(actions[current])[id] = event_time
+    # Print a message for the event
+    print(' '.join(message) + ".")
+    # Log the event
+    log(int(current), id)
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - -
+# Function: Return a string that represents the value of the specified timedelta
+def format_timedelta(td):
+    hours, remainder = divmod(td.seconds, 3600)
+    minutes, seconds = divmod(remainder, 60)
+    message = []
+    if (td.days > 0):
+        message.append("%d %s" % (td.days, phrases.get(language)[PHRASE_DAYS]))
+    if (hours > 0):
+        message.append("%d %s" % (hours, phrases.get(language)[PHRASE_HRS]))
+    if (minutes > 0):
+        message.append("%d %s" % (minutes, phrases.get(language)[PHRASE_MINS]))
+    if (seconds > 0):
+        message.append("%d %s" % (seconds, phrases.get(language)[PHRASE_SECS]))
+    return ' '.join(message)
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - -
+# Function: Log the event
+def log(action, id):
+    # TODO use current date/time in logging
+    # Just print for now
+    ###print('log: {},{},{},{}'.format(datetime.now(), action, id, len(idsPresent)))
+    logger.info('action={},id={},count={}'.format(action, id, len(ids_present)))
+    # Also, "speak" the event
+    speak(action, id)
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - -
+# Function: "Speak" a salutation based on the event and id
+def speak(action, id):
+    # Set the sound file name to "{student_name}_{opt}_{lang}.mp3" (in temp directory)
+    fname = os.path.join(TEMP_DIR, students.get(id).replace(' ', '_') + '_' + str(action) + '_' + language + '.mp3')
+    # If the necessary sound file doesn't already exist, create it
+    if not os.path.isfile(fname):
+        # Phrase to be spoken is "{salutation} {student name}"
+        phrase = phrases.get(language)[action] + ' ' + students.get(id) + '.'
+        try:
+            tts = gTTS(text=phrase, lang=language)
+            # Save spoken sound to file
+            tts.save(fname)
+        except:
+            # Ignore any erros
+            pass
+    # Play the sound file (in a subprocess, so we don't wait while the file plays)
+    try:
+        subprocess.Popen("mpg123 -q " + fname, shell=True)
+    except:
+        # Ignore any errors
+        pass
 
 
 # - - - - - - - - - - - - - - - - - - - - - - - - -
 # Function: First things first...
 def initialize():
     # Ensure temporary directory exists
-    global gTemp
-    gTemp = os.path.abspath(gTemp)
-    os.makedirs(gTemp, exist_ok=True)
+    global TEMP_DIR, data_check
+    TEMP_DIR = os.path.abspath(TEMP_DIR)
+    os.makedirs(TEMP_DIR, exist_ok=True)
     
     # Initialize logging
     logger.setLevel(logging.INFO)
@@ -89,69 +193,174 @@ def initialize():
     handler.setFormatter(formatter)
     # add the handler to the logger
     logger.addHandler(handler)
-
+    
+    # Check for previously saved state
+    if os.path.isfile(STATE_FILE):
+        load_state()
+    
+    # Initialize our checksum
+    check = hashlib.sha1()
     # Read student list from file into our dictionary
-    with open('students.dat', 'r') as datafile:
+    print(phrases.get(language)[PHRASE_DATA_READ], end='')
+    with open(DATA_FILE, 'r') as datafile:
         for line in datafile:
             try:
+                # Update checksum with next line of data
+                check.update(line.encode())
+                # Split the line into two: before/after comma
                 (id, name) = line.split(',')
+                # Add the id/name to the student list; strip extra characters from the end of the name
                 students[id] = name.rstrip()
+                print('.', end='')
             except ValueError:
                 # ValueError occurs if the line does not have two values separated by a comma
                 # If this happens, ignore this line and go on to the next line
                 continue
+    print(phrases.get(language)[PHRASE_DATA_COMPLETE])
+    # Compare checksums
+    #   Note that saved state should be invalidated if the student list changes.
+    #   We calculate a check ('checksum') on the student data as it is loaded, and
+    #   warn that the checksum/data has changed.
+    #   Warning allows the user to decide whether changes to student list
+    #   are ok (e.g. new student) or might cause conflicts with saved state 
+    #   (e.g. id card changed from one student to another).
+    if (data_check != '') and (data_check != check.hexdigest()) and ('ids' in state):
+        print(phrases.get(language)[PHRASE_WARN_DATA_CHECK])
+    data_check = check.hexdigest()
 
-
-# - - - - - - - - - - - - - - - - - - - - - - - - -
-# Function: A student has entered the class
-def enter(id):
-    idsPresent.append(id)
-    print('{} entered the class.'.format(students.get(id)))
-    log(1, id)
-
-
-# - - - - - - - - - - - - - - - - - - - - - - - - -
-# Function: A student has left the class
-def leave(id):
-    idsPresent.remove(id)
-    print('{} left the class.'.format(students.get(id)))
-    log(0, id)
 
 
 # - - - - - - - - - - - - - - - - - - - - - - - - -
-# Function: Log the event
-#   opt = 1 (enter) or  0 (leave)
-def log(opt, id):
-    # Just print for now
-    ###print('log: {},{},{},{}'.format(datetime.now(), opt, id, len(idsPresent)))
-    logger.info('action={},id={},count={}'.format(opt, id, len(idsPresent)))
-    # Also, "speak" the event
-    speak(opt, id)
+# Function: Print the current class register
+def print_state():
+    # Dump list of students present in class
+    print(phrases.get(language)[PHRASE_QUERY].format(len(ids_present)))
+    for id in ids_present:
+        print('  {}'.format(students[id]))
+    print(ids)
+    print(ids_present)
 
 
 # - - - - - - - - - - - - - - - - - - - - - - - - -
-# Function: "Speak" a salutation based on the event and id
-#   opt = 1 (enter) or  0 (leave)
-def speak(opt, id):
-    # Set the sound file name to "{student_name}_{opt}_{lang}.mp3" (in temp directory)
-    fname = os.path.join(gTemp, students.get(id).replace(' ', '_') + '_' + str(opt) + '_' + gLang + '.mp3')
-    # If the necessary sound file doesn't already exist, create it
-    if not os.path.isfile(fname):
-        # Phrase to be spoken is "{salutation} {student name}"
-        phrase = salutations.get(gLang)[opt] + ' ' + students.get(id) + '.'
-        try:
-            tts = gTTS(text=phrase, lang=gLang)
-            # Save spoken sound to file
-            tts.save(fname)
-        except:
-            # Ignore any erros
-            pass
-    # Play the sound file (in a subprocess, so we don't wait while the file plays)
+# Function: Save variables to a file
+def save_state():
+    global state
+    # Show message indicating whether there are students present in class
+    if len(ids_present) > 0:
+        print(phrases.get(language)[PHRASE_STATE_NOT_EMPTY])
+    else:
+        print(phrases.get(language)[PHRASE_STATE_EMPTY])
+    # Save important variables in state dictionary
+    state['data_check'] = data_check
+    # Only save ids/actions/events if actions have occurred
+    if (ids[actions[0]]) or (ids[actions[1]]):
+        state['ids'] = ids
+        print(phrases.get(language)[PHRASE_STATE_SAVE])
+    else:
+        # Otherwise, ensure ids/actions/events are not saved
+        if ('ids' in state):
+            del state['ids']
     try:
-        subprocess.Popen("mpg123 -q " + fname)
+        # Open the state file for writing
+        state_file = open(STATE_FILE, 'w')
+        # Save the dictionary to file
+        json.dump(state, state_file, cls=JSONDateTimeEncoder)
     except:
-        # Ignore any errors
-        pass
+        # If there are any errors, reset
+        print(phrases.get(language)[PHRASE_ERR_STATE_SAVE])
+        clear_state()
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - -
+# Function: Load variables from a file
+def load_state():
+    global state, data_check, ids, ids_present
+    try:
+        # Open the state file for reading
+        state_file = open(STATE_FILE, 'r')
+        # Load the contents of the file into a dictionary
+        state = json.load(state_file, cls=JSONDateTimeDecoder)
+        # Load variables from dictionary
+        data_check = state['data_check']
+        if ('ids' in state):
+            # Only display message if ids/actions/events are being restored
+            print(phrases.get(language)[PHRASE_STATE_LOAD])
+            ids.update(state['ids'])
+            # (must also update dictionary view)
+            ids_present = ids[actions[1]].keys()
+    except:
+        # If there are any errors, reset
+        print(phrases.get(language)[PHRASE_ERR_STATE_READ])
+        clear_state()
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - -
+# Function: Clear current state
+def clear_state():
+    global state, ids, ids_present
+    # clear state and ids dictionaries
+    state.clear()
+    ids = {key: {} for key in actions}
+    # (must also update dictionary view)
+    ids_present = ids[actions[1]].keys()
+    # remove state file
+    try:
+        if os.path.isfile(STATE_FILE):
+            os.remove(STATE_FILE)
+        # TODO remove graph log file
+        ###handler = logging.FileHandler('rfcr_graph.log')
+    except:
+        # If there are any errors, advise user
+        print(phrases.get(language)[PHRASE_ERR_STATE_FILE])
+    else:
+        print(phrases.get(language)[PHRASE_RESET])
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - -
+# Class: Allow conversion from datetime variable to string
+#        Used when saving the current state to file
+#        see: https://gist.github.com/abhinav-upadhyay/5300137
+class JSONDateTimeDecoder(json.JSONDecoder):
+
+    def __init__(self, *args, **kargs):
+        JSONDecoder.__init__(self, object_hook=self.dict_to_object,
+                             *args, **kargs)
+    
+    def dict_to_object(self, d): 
+        if '__type__' not in d:
+            return d
+
+        type = d.pop('__type__')
+        try:
+            dateobj = datetime(**d)
+            return dateobj
+        except:
+            d['__type__'] = type
+            return d
+
+# - - - - - - - - - - - - - - - - - - - - - - - - -
+# Class: Allow conversion from string variable to datetime
+#        Used when loading the current state from file
+class JSONDateTimeEncoder(JSONEncoder):
+    """ Instead of letting the default encoder convert datetime to string,
+        convert datetime objects into a dict, which can be decoded by the
+        DateTimeDecoder
+    """
+        
+    def default(self, obj):
+        if isinstance(obj, datetime):
+            return {
+                '__type__' : 'datetime',
+                'year' : obj.year,
+                'month' : obj.month,
+                'day' : obj.day,
+                'hour' : obj.hour,
+                'minute' : obj.minute,
+                'second' : obj.second,
+                'microsecond' : obj.microsecond,
+            }   
+        else:
+            return JSONEncoder.default(self, obj)
 
 
 # = = = = = = = = = = = = = = = = = = = = = = = = =
